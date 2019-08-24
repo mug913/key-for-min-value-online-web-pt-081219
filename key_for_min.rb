e849b376d648e42242bb21248ad98c7bcee0ddb7
Brad Alexander <brad.alexander913@gmail.com>
2019-08-24 23:12:22 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
+
+require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+comp_k = nil
+name_hash.each do |k, v| 
+  if ((comp_k) && (name_hash[k] < name_hash[comp_k]))
+    comp_k = k 
+  elsif !(comp_k)
+    comp_k = k 
+  end 
+  end
+  return comp_k 
 end
